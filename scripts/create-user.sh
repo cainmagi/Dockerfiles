99f8215b0da2570012a6ba27335b0a9698adf66b
Yuchen Jin <cainmagi@gmail.com>
2025-04-25 16:31:36 -0500

    1.0.1
    Fix: Correct the path of the SSL configuration file.

--- scripts/create-user.sh
+++ scripts/create-user.sh
@@ -94,7 +94,8 @@ if [ "x${LANG_NAME}" = "xzh_CN" ] && [ "x${LANG_ENC}" = "xUTF-8" ]; then
   cp -f ${SCRIPTPATH}/etc/locale-zh_CN.gen /etc/locale.gen || fail
 fi
 cp -f ${SCRIPTPATH}/etc/sudoers /etc/ || fail
-cp -f ${SCRIPTPATH}/etc/ssl/code-server.cnf /etc/ || fail
+mkdir -p /etc/ssl/ || fail
+cp -f ${SCRIPTPATH}/etc/ssl-code-server.cnf /etc/ssl/code-server.cnf || fail
 
 # Setting language
 apt-get ${APT_OPTIONS} install -y locales || fail
